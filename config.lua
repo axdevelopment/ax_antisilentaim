f9d47db6d5a78b987d24757680f1d9e20b4c75c3
Ardo <90957355+axdevelopment@users.noreply.github.com>
2022-12-03 19:09:24 +0100

    Update config.lua
    moved webhook to server.lua (security improvement)

--- config.lua
+++ config.lua
@@ -1,5 +1,4 @@
 Config = {}
 Config.Framework = 'qb' -- Framework: qb or esx
-Config.Webhook = '' -- Discord Bot Hook
 Config.Avatar = 'https://via.placeholder.com/200x200'
 Config.DropStaff = true -- Drop staff if they are using modified RPF files
